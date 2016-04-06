a56d1b70bcf6c8d3dd544139b8a8b1304c9faeea
thiasman <mathias.canales@gmail.com>
2016-04-06 17:21:28 +0100

    Can't send anything if no connexion

--- Client/src/main/java/client/BasicClient.java
+++ Client/src/main/java/client/BasicClient.java
@@ -25,28 +25,31 @@ public class BasicClient {
 	private SimpleUser user;
 	private final String serverHostname = new String ("127.0.0.1");
 	private final int serverPort = 10008;
-	
+
 	Socket echoSocket;
 	ObjectOutputStream outputStream =  null;
 	ObjectInputStream inputStream =  null;
-	
+
 	private ThreadListener clientThreadListener = null;
 	private ThreadInputListener clientThreadInputListener = null;
-	
+
 	private String storedPassword;
-	
+
+	private boolean isConnectedToServer=false;
+
 	public BasicClient(String name, String pwd) {
 		System.out.println ("Attemping to connect to host " + serverHostname + " on port "+ serverPort + " .");
-		
+
 		setUser(new SimpleUser(name));
-		
+
 		storedPassword = pwd;
-		
+
 		clientThreadInputListener = new ThreadInputListener();
 		clientThreadInputListener.start();
-		
+
 		if(initializeBuffer()){
 			connectToServer();
+			isConnectedToServer = true;
 		}
 		else{
 			System.out.println ("Press C to to try to connect to the server");
@@ -55,10 +58,10 @@ public class BasicClient {
 
 	private void connectToServer() {
 		logon(storedPassword);
-		
+
 		clientThreadListener = new ThreadListener();
 		clientThreadListener.start(); 
-		
+
 		System.out.println ("Hello "+ getUser().getName());
 
 		DisplayMenu();
@@ -82,22 +85,22 @@ public class BasicClient {
 		}
 
 	}
-	
+
 	public boolean initializeBuffer(){
 		try {
 			echoSocket = new Socket(serverHostname, serverPort);
 			outputStream =  new ObjectOutputStream(echoSocket.getOutputStream());
 			inputStream =  new ObjectInputStream(echoSocket.getInputStream());
 			return true;
-			} catch (UnknownHostException e) {
+		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host: " + serverHostname);
-//			System.exit(1);
+			//			System.exit(1);
 			return false;
 		} catch (IOException e) {
 			System.err.println("Couldn't get I/O for "
 					+ "the connection to: " + serverHostname);
 			return false;
-//			System.exit(1);
+			//			System.exit(1);
 		}
 	}
 
@@ -113,7 +116,7 @@ public class BasicClient {
 		System.out.println ("Press S to send a message to a user");
 		System.out.println ("Press Q to disconnect");
 	}
-	
+
 	class ThreadListener extends Thread 
 	{
 		private boolean running = true;
@@ -123,7 +126,7 @@ public class BasicClient {
 			while(running){
 				try {
 					Message message = (Message) inputStream.readObject();
-					
+
 					switch(message.getMessageType()){
 					case LOGON:
 						System.out.println("You shouldn't receive this message");
@@ -142,22 +145,24 @@ public class BasicClient {
 						break;
 					case QUIT:
 						System.out.print("Server down: "); 
+						isConnectedToServer = false;
 						break;
 					case LIST_USERS:
 						DisplayListUsers(((MessageUsersList) message).getClientsList());
 						break;
 					}
-					
+
 				} catch (ClassNotFoundException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
+					isConnectedToServer = false;
 					System.out.println("Connexion lost");
 					System.out.println ("Press C to to try to connect to the server");
 					stopThread();
 				}
 			}
 		}
-		
+
 		public void stopThread(){
 			running=false;
 		}
@@ -171,66 +176,70 @@ public class BasicClient {
 		{
 			String userInput = null;
 			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-			
+
 			while(running){
 				try {
 					while ((userInput = stdIn.readLine()) != null ) 
 					{
 						switch(userInput){
-							case "T":
+						case "T":
+							if(isConnectedToServer){
 								AdminMessage time = new AdminMessage(Message.MessageTypes.TIME);
-								outputStream.writeObject(time);
-								break;
-							case "L":
+								outputStream.writeObject(time);}
+							break;
+						case "L":
+							if(isConnectedToServer){
 								MessageUsersList list = new MessageUsersList();
-								outputStream.writeObject(list);
-								break;
-							case "S":
+								outputStream.writeObject(list);}
+							break;
+						case "S":
+							if(isConnectedToServer){
 								System.out.println ("Write his username and press enter");
 								String username = stdIn.readLine();
 								System.out.println ("Write your message and press enter");
 								String mess = stdIn.readLine();
 								BasicMessage basicMessage = new BasicMessage(Message.MessageTypes.BASIC_MESSAGE, mess, username, getUser().getName());
-								outputStream.writeObject(basicMessage);
-								break;
-							case "Q":
+								outputStream.writeObject(basicMessage);}
+							break;
+						case "Q":
+							if(isConnectedToServer){
 								AdminMessage quit = new AdminMessage(Message.MessageTypes.QUIT);
 								outputStream.writeObject(quit);
 								logout();
-								clientThreadListener.stopThread();
-								break;
-							case "C":
-								if(initializeBuffer()){
-									connectToServer();
-								}
-								else{
-									System.out.println ("Press C to to try to connect to the server");
-								}
-								break;
-							default:
-								System.out.println ("Wrong input");
-								break;
+								clientThreadListener.stopThread();}
+							break;
+						case "C":
+							if(initializeBuffer()){
+								connectToServer();
+							}
+							else{
+								System.out.println ("Press C to to try to connect to the server");
+							}
+							break;
+						default:
+							System.out.println ("Wrong input");
+							break;
 						}
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
-			
+
 		}
-		
+
 		public void stopThread(){
 			running=false;
 		}
 	}
-	
+
 	private void DisplayListUsers(Vector<String> clientsList) {
 		Iterator<String> i = clientsList.iterator();
-	    while (i.hasNext()) {
-	      System.out.println(i.next());
-	    }
+		while (i.hasNext()) {
+			System.out.println(i.next());
+		}
 	}
-	
+
 	public SimpleUser getUser() {
 		return user;
 	}
@@ -245,5 +254,13 @@ public class BasicClient {
 
 	public void setClientThreadListener(ThreadListener clientThreadListener) {
 		this.clientThreadListener = clientThreadListener;
+	}
+
+	public boolean isConnectedToServer() {
+		return isConnectedToServer;
+	}
+
+	public void setConnectedToServer(boolean isConnectedToServer) {
+		this.isConnectedToServer = isConnectedToServer;
 	}
 }
